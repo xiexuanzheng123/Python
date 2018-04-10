77d2c172a347143c7de3ec6db535b3f40e2e9f97
xiexuanzheng123 <349328984@qq.com>
2018-04-10 16:14:08 +0800

    test

---
+++ test.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+str = 'Runoob'
+print(str)
+print(str[0:-1])  # 输出第一个到倒数第二个的字符
+print(str[0])
+print(str[2:5])
+print(str * 2)
+print(str + '你好')
+
+print('----------------')
+
+print('hello\nrunoob')
+print(r'hello\nrunoob')
+
+# input("\n\n按下 enter 键后退出。")
+
+import sys; x = 'runoob'; sys.stdout.write(x + '\n')
+
+
